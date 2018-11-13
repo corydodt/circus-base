9b06e3d5d85c679a76f638c2e6b5c4a7eefa0524
Cory Dodt <corydodt@gmail.com>
2018-11-13 15:40:34 -0800

    #1 ws

--- doc/example/py2prog/python2_3_hook.py
+++ doc/example/py2prog/python2_3_hook.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import platform
 
 
-# this file must be executable in python3 to use a hook from here! 
+# this file must be executable in python3 to use a hook from here!
 def before_start(watcher, arbiter, hook_name, **kwargs):
     print("hello before_start hook for Python %s" % platform.python_version())
     return True
